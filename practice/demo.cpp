a4e7e2d75c59e00f759eb4d8d0227ec4b0af5112
dinesh-polimera <pdineshsampathram@gmail.com>
2026-03-11 09:31:59 +0530

    Add files via upload

---
+++ practice/demo.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        pair<int,int> p;
+        cin>>p.first>>p.second;
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+}
