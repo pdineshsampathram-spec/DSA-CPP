ef2dfe3266aef70a4149cb808b72c314db0b0e95
dinesh-polimera <pdineshsampathram@gmail.com>
2026-03-11 19:55:55 +0530

    Add files via upload

--- vector practice/demo.cpp
+++ vector practice/demo.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    for(auto k:v){
+        cout<<k<<" ";
+    }
+}
