5cc0e311a53540581b8ccb6c3c74ac5d8b326b3f
dinesh-polimera <pdineshsampathram@gmail.com>
2026-03-12 19:10:03 +0530

    Add files via upload

---
+++ vector practice/demo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> v;
+    for (int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        if(find(v.begin(),v.end(),x)==v.end()){
+            v.push_back(x);
+        }
+    }
+    for(auto k:v){
+        cout<<k<<" ";
+    }
+
+}
