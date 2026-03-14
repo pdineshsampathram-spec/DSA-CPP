4018c5f0b4ca22529ffffcb3bb06349b80620014
dinesh-polimera <pdineshsampathram@gmail.com>
2026-03-14 11:29:28 +0530

    Update 2341. Maximum Number of Pairs in Array.cpp

--- LeetCode/Easy/2341. Maximum Number of Pairs in Array.cpp
+++ LeetCode/Easy/2341. Maximum Number of Pairs in Array.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> numberOfPairs(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int pair=0;
-        int i=0;
         for(int i=1;i<nums.size();i++){
             if(nums[i-1]==nums[i]){
                 i=i+1;
