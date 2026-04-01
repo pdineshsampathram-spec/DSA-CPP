1dcd8831e92900f97991ec1d9dee8cf3b4219236
dinesh-polimera <pdineshsampathram@gmail.com>
2026-04-01 14:48:15 +0530

    Update 1. Two Sum.cpp

--- LeetCode/Easy/1. Two Sum.cpp
+++ LeetCode/Easy/1. Two Sum.cpp
@@ -1,18 +1,27 @@
+/*
+🔹 Approach: Brute Force
+
+Check all pairs (i, j) where j > i.
+If nums[i] + nums[j] == target, return indices.
+
+⏱ Time: O(n^2)
+💾 Space: O(1)
+
+📌 Simple but not optimal (use hashmap for O(n)).
+*/
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v(2);
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(i==j){
-                    continue;
-                }
-                else{
-                    if(nums[i]+nums[j]==target){
-                        v[0]=i;
-                        v[1]=j;
-                        return v;
-                    }
+        vector<int> v(2,0);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    v[0] = i;
+                    v[1] = j;
+                    return v;
                 }
             }
         }
